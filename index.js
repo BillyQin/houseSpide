1043505fc9728380737dfa83dfd7c57fbb142cd1
BillyQin <965824981@qq.com>
2018-03-20 21:35:16 +0800

    fix encode gbk

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var url = require('url');  // 解析操作url
-var superagent  = require('superagent'); // ajax使用的http库
 var cheerio  = require('cheerio'); // 服务器端的jquery,获取html
+var iconv = require('iconv-lite');
+var charset = require('superagent-charset');
+var superagent = charset(require('superagent')); // ajax使用的http库
 
 var app = express();
 const port = 3000;
@@ -10,19 +12,26 @@ const port = 3000;
 var targetUrl = 'http://www.hzfc.gov.cn/yszmore.php'; // 政府房产网
 
 var content = '';
+let $ = '';
 
 superagent
   .get(targetUrl)
-  // .set('charset', 'utf-8')
-  .charset('gbk')
+  .charset() // 自动检测编码
   .end(function (err, res) {
   content = res
 });
 
 app.get('/', function (req, res) {
+  // $ = cheerio.load(content);
+  // let text = $('.policy');
+  // console.log(text);
   res.send(content);
 });
 
 var server = app.listen(port, function () {
   console.log('Example app listening at http://localhost:%s', port);
 });
+
+// function getNoticeList() {
+//   let result = [];
+// }
