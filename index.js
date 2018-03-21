444d22fd985f522a69342c745b0f6e09065ae4e5
BillyQin <965824981@qq.com>
2018-03-21 10:08:55 +0800

    fix get data

--- index.js
+++ index.js
@@ -10,27 +10,28 @@ const port = 3000;
 
 // 透明售房网 http://www.tmsf.com/index.jsp
 var targetUrl = 'http://www.hzfc.gov.cn/yszmore.php'; // 政府房产网
-
-function getNoticeList() {
-  // let result = [];
-  let text = $('.policy');
-  console.log(text);
-}
+// var targetUrl = 'https://cnodejs.org';
 
 function getSuperagent(resquest) {
   superagent
   .get(targetUrl)
   .charset() // 自动检测编码
   .end((err, res) => {
-    resquest.send(JSON.stringify(res.body));
-    const $ = cheerio.load(res.text);
-    console.log($('.top'));
+    var $ = cheerio.load(res.text);
+    var items = [];
+    $('.policy a').each(function (idx, element) {
+      var $element = $(element);
+      items.push(
+        $element.attr('href')
+      );
+    });
+    items.push({length: items.length});
+    resquest.send(items);
   });
 }
 
 app.get('/', function (req, res) {
   getSuperagent(res);
-  // res.send(content);
 });
 
 var server = app.listen(port, function () {
