3394650f76d9ccdf2db1a0cc06e87bf07f1b5c1e
BillyQin <965824981@qq.com>
2018-03-24 22:19:43 +0800

    get subpages detail

--- index.js
+++ index.js
@@ -9,29 +9,52 @@ var app = express();
 const port = 3000;
 
 // 透明售房网 http://www.tmsf.com/index.jsp
-var targetUrl = 'http://www.hzfc.gov.cn/yszmore.php'; // 政府房产网
+const targetUrl = 'http://www.hzfc.gov.cn/yszmore.php'; // 政府房产网
 // var targetUrl = 'https://cnodejs.org';
 
-function getSuperagent(url, resquest) {
-  superagent
-  .get(url)
-  .charset() // 自动检测编码
-  .end((err, res) => {
-    var $ = cheerio.load(res.text);
-    var items = [];
-    $('.policy a').each((idx, element) => {
-      var $element = $(element);
-      items.push(
-        $element.attr('href')
-      );
+let getHtml = (url) => {
+  return new Promise((resolve, reject) => {
+    superagent
+    .get(url)
+    .charset() // 自动检测编码
+    .end((err, res) => {
+      resolve(cheerio.load(res.text));
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
     });
-    items.push({length: items.length});
-    resquest.send(items);
-  });
+  })
 }
 
-app.get('/', function (req, res) {
-  getSuperagent(targetUrl, res);
+let pageList = ($) => {
+  const pages = new Map();
+  $('.policy a').each((idx, element) => {
+    const href = $(element).attr('href');
+    if (href.includes('pid')) {
+      pages.set(idx, href);
+    }
+  });
+  return [...pages.values()];
+}
+
+let getPageDetail = async (pageList) => {
+  let pageInfo = [];
+  pageList.forEach(async (item, index) => {
+    let newPage = await getHtml(`http://www.hzfc.gov.cn/${item}`).catch(err => console.log(err));
+    let attr = newPage('TABLE p.style1').text();
+    console.log(index, attr);
+    pageInfo.push(attr);
+  });
+  return pageInfo;
+}
+
+app.get('/', async (req, res) => {
+  const $ = await getHtml(targetUrl).catch(err => console.log(err));
+  let list = pageList($);
+  let pageInfo = await getPageDetail(list);
+  console.log(pageInfo);
+  res.send(pageInfo);
 });
 
 var server = app.listen(port, function () {
